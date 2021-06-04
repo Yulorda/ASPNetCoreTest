14c9bf8a69e4b86db95f256cbece3c99d1f0d4f7
Yulorda <xsxsmile@gmail.com>
2021-06-04 21:49:30 +0500

    Request Run

--- ASPNetCoreTest/ASPNetCoreTest/Startup.cs
+++ ASPNetCoreTest/ASPNetCoreTest/Startup.cs
@@ -25,38 +25,11 @@ namespace ASPNetCoreTest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
-
-            //app.UseHttpsRedirection();
-            //app.UseStaticFiles();
-
-            //app.UseRouting();
-
-            //app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapRazorPages();
-            //});
-
-            //После обновления 8/16/32/64/128
-            ///и компоненты middleware создаются один раз и живут в течение всего жизненного цикла приложения. 
-            //То есть для последующей обработки запросов используются одни и те же компоненты.
-
-            int x = 2;
+            //Run не передает запрос дальше
+            //Устанавливать этот метод вконце обработки
             app.Run(async (context) =>
             {
-                x = x * 2;  //  2 * 2 = 4
-                await context.Response.WriteAsync($"Result: {x}");
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
