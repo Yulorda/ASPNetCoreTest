500ba71da4df5d693589264793e3525b0bb07aa2
Yulorda <xsxsmile@gmail.com>
2021-06-04 21:45:45 +0500

    Test midlware lifecycle

--- ASPNetCoreTest/ASPNetCoreTest/Startup.cs
+++ ASPNetCoreTest/ASPNetCoreTest/Startup.cs
@@ -25,34 +25,39 @@ namespace ASPNetCoreTest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             //app.UseEndpoints(endpoints =>
             //{
             //    endpoints.MapRazorPages();
             //});
 
-            app.UseEndpoints(endpoints =>
-                endpoints.MapGet("/", async context => 
-                { 
-                    await context.Response.WriteAsync($"RootPath:{env.ContentRootPath}"); 
-                }));
+            //ѕосле обновлени€ 8/16/32/64/128
+            ///и компоненты middleware создаютс€ один раз и живут в течение всего жизненного цикла приложени€. 
+            //“о есть дл€ последующей обработки запросов используютс€ одни и те же компоненты.
+
+            int x = 2;
+            app.Run(async (context) =>
+            {
+                x = x * 2;  //  2 * 2 = 4
+                await context.Response.WriteAsync($"Result: {x}");
+            });
         }
     }
 }
