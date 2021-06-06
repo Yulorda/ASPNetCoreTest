3169db70ff11d20e230849ca40009145cf266de0
Yulorda <xsxsmile@gmail.com>
2021-06-06 11:27:38 +0500

    Catch exceptions

--- ASPNetCoreTest/ASPNetCoreTest/Startup.cs
+++ ASPNetCoreTest/ASPNetCoreTest/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System.IO;
 
@@ -26,23 +25,48 @@ namespace ASPNetCoreTest
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         //IWebHostEnvironment можно добавить к методу конфиг еще один параметр
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseFileServer();
-            //app.UseFileServer(enableDirectoryBrowsing: true);
-
-            app.UseFileServer(new FileServerOptions
+            //env.EnvironmentName = "Production";
+            if (env.IsDevelopment())
             {
-                EnableDirectoryBrowsing = true,
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\html")),
-                RequestPath = new PathString("/pages"),
-                EnableDefaultFiles = false
-            });
-
-            app.Run(async (context) =>
+                app.UseDeveloperExceptionPage();
+                //app.UseStatusCodePages();
+                //app.UseStatusCodePages("text/plain", "Error. Status code : {0}");
+                //app.UseStatusCodePagesWithRedirects("/error?code={0}");
+            }
+            else
             {
-                await context.Response.WriteAsync("Hello World");
-            });
+                app.UseExceptionHandler("/error");
+            }
+
+            #region TestAppUseStatusCodePagesWithReExecute
+
+            app.UseStatusCodePagesWithReExecute("/error", "?code={0}");
+
+            app.Map("/error", ap => ap.Run(async context =>
+            {
+                await context.Response.WriteAsync($"Err: {context.Request.Query["code"]}");
+            }));
+
+            app.Map("/hello", ap => ap.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hello ASP.NET Core");
+            }));
+
+            #endregion TestAppUseStatusCodePagesWithReExecute
+
+            //app.Map("/error", ap => ap.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("DivideByZeroException occured!");
+            //}));
+
+            //app.Run(async (context) =>
+            //{
+            //    int x = 0;
+            //    int y = 8 / x;
+            //    await context.Response.WriteAsync($"Result = {y}");
+            //});
         }
     }
 }
